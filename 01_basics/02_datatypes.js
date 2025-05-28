6c7d56e59f8e71efd1e83ada1fcd7a145fdaf16f
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-28 13:25:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // Treat all java code as newer version
+
+//alert(3+3) //We are using nodejs , not browser
+console.log(3 +
+    3           //Console readability should be high //console.log(3+3)
+)
+
+let name="Vishal Sharma" //string data type
+let age =21  //number
+let isLoggedIn= true/false  //Boolean  
+
+//bigInt data type
+//string =>""
+//boolean=> true/false
+//number 
+//null =>standalone value
+//undefined 
+//symbol => for uniqueness
+
+//object
+
+
+console.log(typeof undefined)//undefined
+console.log(typeof null )//object 
