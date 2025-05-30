7fbf7468a8ebc20639c2e535f638ba94b3d2be1f
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-30 13:20:15 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,3 +6,26 @@
 //2 Reference(Non primitive)
 
 //-- Array , Objects,Functions
+
+
+//+++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive ) , Heap(Non-Primitive )
+
+let myYoutubename="Vishaldotcom"
+let anothername = "Sharmaji"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"Vishalsharma@gmail.com",
+    upi: "user@ybl"
+
+
+}
+let userTwo = userOne
+
+userTwo.email ="Vishalsharma8950@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
