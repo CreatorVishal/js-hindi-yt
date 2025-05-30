7174dda97aca575942cd4e96fdba0d967b795fd3
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-30 12:40:20 +0000

    operations

--- 01_basics/03_conversionorOperations.js
+++ 01_basics/03_conversionorOperations.js
@@ -1,25 +1,53 @@
-let score= "55"
+// let score= "55"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score) 
-console.log(typeof valueInNumber)
-  //"33" => 33
-  //"33aba" => Nan
-  //true =>1; false =>0
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber)
+//   //"33" => 33
+//   //"33aba" => Nan
+//   //true =>1; false =>0
 
-  let isLoggedIn =1
-  let  booleanIsLoggedIn = Boolean(isLoggedIn);
-  console.log(booleanIsLoggedIn)  
+//   let isLoggedIn =1
+//   let  booleanIsLoggedIn = Boolean(isLoggedIn);
+//   console.log(booleanIsLoggedIn)  
 
   //1 => true ; 0 => false
   //""=>false
   //"Vishal"=>true
 
-  let someNumber = 23
-  let stringNumber= String(someNumber)
-  console.log(stringNumber);
-  console.log(typeof stringNumber);
-  
+//   let someNumber = 23
+//   let stringNumber= String(someNumber)
+//   console.log(stringNumber);
+//   console.log(typeof stringNumber);
+
+
+
+  // ******************************* Operations ********************************
+
+  let value =3
+  let negValue =- value
+  console.log(negValue);
+
+//   console.log(2+2);
+//   console.log(2-2);
+//   console.log(2*2);
+//   console.log(2**2);
+//   console.log(2/2); 
+//   console.log(2%2);
+
+let str1= "hello"
+let str2="Vishal"
+let str3= str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); // ==> 122
+console.log(1+2+"2"); // ==>32
+ 
+
+
   
