72b69d7b6d7c3c03bbb4ef052346dfc06125d611
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-30 12:53:33 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+// === check value and also their data type
+
+console.log("2"===2);
+
+console.log("2">1);
