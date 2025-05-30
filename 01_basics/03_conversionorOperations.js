c35904b25824e035b9d9d6bd24318cdbe835f7a9
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-30 12:21:32 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionorOperations.js
@@ -0,0 +1,25 @@
+let score= "55"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber)
+  //"33" => 33
+  //"33aba" => Nan
+  //true =>1; false =>0
+
+  let isLoggedIn =1
+  let  booleanIsLoggedIn = Boolean(isLoggedIn);
+  console.log(booleanIsLoggedIn)  
+
+  //1 => true ; 0 => false
+  //""=>false
+  //"Vishal"=>true
+
+  let someNumber = 23
+  let stringNumber= String(someNumber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
+  
+  
