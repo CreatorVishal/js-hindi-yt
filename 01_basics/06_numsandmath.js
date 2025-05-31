9d583efc466aed4999a354cb1246f22a5f58b90f
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-31 07:34:43 +0000

    math and num

---
+++ 01_basics/06_numsandmath.js
@@ -0,0 +1,30 @@
+const score=440
+// console.log(score); // 440
+
+const balance = new Number(100)
+// console.log(balance); //[Number:100]
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber =123.89
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3)); //4 
+// console.log(Math.round(4.7)); //5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9));//4
+
+console.log(Math.random());
+
+
+
