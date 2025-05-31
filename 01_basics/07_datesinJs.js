120f882527c064bf17074cc350aac7d13b7ede0d
CreatorVishal <Vishalsharma805966@gmail.com>
2025-05-31 08:21:05 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,19 @@
+//Dates
+let myDate = new Date ()
+// console.log(myDateString)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+
+// console.log(typeof myDate);
+
+
+
+// let myCreatedDate = new Date(2023,0,23) //moths start from -->0
+// let myCreatedDate = new Date(2023,0,23,5,3) 
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2025")
+console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate.toDateString());
+
+
