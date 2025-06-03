6fe1022f3d25d8b2b4c44f364120ed4495ea6c97
CreatorVishal <Vishalsharma805966@gmail.com>
2025-06-03 08:18:29 +0000

    while loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,55 @@
+// // FOR LOOP
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i; 
+//     console.log(element);   
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`outer loop value : ${i}`);
+//     for(let j =1; j<=10;j++){
+//         // console.log(`inner loop value ${j} and inner loop ${i}`);
+//         // console.log(i+'*'+j+' = '+(i*j));
+        
+
+
+// //     }
+    
+    
+// // }
+
+// let myArray =["flash ","batman","Superman"]
+
+// // for(let i=0;i<myArray.length;i++){
+// //     // console.log(myArray[i]);
+// // }
+//     }
+
+// }
+
+// // Two important keyword 
+// // 1.break
+// //2. continue 
+
+// --------------------------------
+
+// for(let index=1;index<=20;index++){
+//     if(index==5){
+//         console.log(`detected 5`);
+//         break
+        
+//     }
+//     console.log(`Value of index is ${index} `);
+    
+// }
+// ---------------------------------------------
+for(let index=1;index<=20;index++){
+    if(index==5){
+        console.log(`detected 5`);
+        continue // 
+        
+    }
+    console.log(`Value of index is ${index} `);
+    
+}
