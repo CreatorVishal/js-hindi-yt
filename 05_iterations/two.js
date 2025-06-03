4307e3d0ac751d65775dfe8e8c287cc3ade3a641
CreatorVishal <Vishalsharma805966@gmail.com>
2025-06-03 08:19:11 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,40 @@
+//While Loop
+
+// while (condition) {
+    
+// }
+
+let index =0
+while(index<=10){
+    console.log(`value of index is ${index}  `);
+    index++;
+    
+}
+
+let myArray=["Hitesh","Vishal","Sandy"]
+let arr=0
+while(arr<myArray.length){
+    console.log((`value is ${myArray[arr]}`));
+    arr++;
+    
+}
+
+//Do While Loop
+
+// let score =1
+
+// do{
+//     console.log(`score is ${score}`);
+//     score++;
+// }
+// while(score<=10);
+
+// -------------
+
+let score =11
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+}
+while(score<=10);
